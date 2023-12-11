45d39b2e3711a2be44640f5be1b7b24283758e4e
tdh <tdhster@gmail.com>
2023-12-11 16:45:54 +0300

    working subprocess for mailconverter in sandbox

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-#uvicorn main:app --reload
+# uvicorn main:app --reload
 
 
 from fastapi import FastAPI, UploadFile, File, HTTPException
@@ -19,21 +18,39 @@ logger = logging.getLogger()
 
 app = FastAPI()
 
-# path_to_mailconverterprox = "C:\Program Files\CoolUtils\TotalMailConverterProX\MailConverterProX64.exe"
-path_to_mailconverterprox = "C:\\Program Files\\CoolUtils\\TotalMailConverterProX\\MailConverterProX64.exe"
+path_to_mailconverterprox = r'C:\Program Files\CoolUtils\TotalMailConverterProX\MailConverterProX64.exe'
+path_to_mailconverterprox = path_to_mailconverterprox.replace(" ", "` ")
+
+
+# path_to_mailconverterprox = '"C:\\Program Files\\CoolUtils\\TotalMailConverterProX\\MailConverterProX64.exe"'
 # path_to_mailconverterprox = Path("C:\\","Program Files","CoolUtils","TotalMailConverterProX","MailConverterProX64.exe")
 # MailConverterProX64.exe "<source>" "<destination>" <options>
+# "C:\Program Files\CoolUtils\TotalMailConverterProX\MailConverterProX.exe"
+
+# "C:\Program Files\CoolUtils\TotalMailConverterProX\MailConverterProX64.exe"
+# "C:\Users\tdh\Downloads\На набережных Сочи щас ни-ко-го….eml" "C:\Users\tdh\Downloads\output.pdf" -c PDF
 
 def run_command(input_file_path, output_file_path, additional_params):
     # Replace 'your_program' with the actual command line program and arguments
-    command = [str(path_to_mailconverterprox), input_file_path, output_file_path] + additional_params
+    # command = [str(path_to_mailconverterprox), f'"{input_file_path}"', f'"{output_file_path}"'] + additional_params
+    command = [path_to_mailconverterprox, input_file_path.replace(" ", "` "),
+               output_file_path.replace(" ", "` ")] + additional_params
 
     try:
-        print(path_to_mailconverterprox)
+        # 'C:\Users\tdh\Downloads\mail.eml'
+
+        command = [r'C:\Program` Files\CoolUtils\TotalMailConverterProX\MailConverterProX64.exe']
+                   # r'C:\Users\tdh\Downloads\mail.eml',
+                   # r'C:\Users\tdh\Downloads\mail.eml.pdf',
+                   # r'-c PDF']
+        # print(path_to_mailconverterprox)
         print(str(" ".join(command)))
 
+        # subprocess.run(command, encoding='cp1251')
         subprocess.run(command, check=True)
+        # subprocess.call(command)
     except subprocess.CalledProcessError as e:
+        print(f'Error occured: {e}')
         logger.error(f"Command '{' '.join(command)}' failed with error: {e}")
         raise RuntimeError(f"Command '{' '.join(command)}' failed with error: {e}")
 
@@ -44,7 +61,7 @@ async def upload_file(file: UploadFile = File(...), additional_params: str = "")
         # Create a temporary directory to store files
         with tempfile.TemporaryDirectory() as temp_dir:
             file_path = f"{temp_dir}\\{file.filename}"
-
+            print(f'{file_path=}')
             # Save the uploaded file to the temporary directory
             with open(file_path, "wb") as temp_file:
                 shutil.copyfileobj(file.file, temp_file)
@@ -52,12 +69,15 @@ async def upload_file(file: UploadFile = File(...), additional_params: str = "")
             # Run the command-line program using the uploaded file and additional parameters
             output_file_path = f"{temp_dir}\\{file.filename}.pdf"
             logger.info(f"Received file: {file.filename}")
+            print(f'{file_path=}')
+            print(f'{output_file_path=}')
+            print(f'{additional_params.split()=}')
 
             run_command(file_path, output_file_path, additional_params.split())
 
             # Return the resulting file
-            with open(output_file_path, "rb") as result_file:
-                return {"result_file": result_file.read()}
+            # with open(output_file_path, "rb") as result_file:
+            #     return {"result_file": result_file.read()}
+            return {"result_file": 'ы'}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to process file: {str(e)}")
-
